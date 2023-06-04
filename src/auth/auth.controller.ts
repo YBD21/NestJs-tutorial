b12769825e2681d1cc3519be2c4bd5a061858b03
YBD21 <santosh.deuja2000@gmail.com>
2023-06-04 19:37:13 +0545

    added variable type validation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,21 +1,33 @@
-import { Controller, Post } from "@nestjs/common";
-import { AuthService } from "./auth.service";
+import { Body, Controller, ParseIntPipe, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto';
 
 // post request route : /auth/signup
 
 @Controller('auth')
 export class AuthController {
-    constructor(private authService : AuthService) {}
-        // dependencies injection
-        //   this.authService.login();
-     // post request  
-     @Post('signup')
-     signup(){
-        return this.authService.signup();
-     };
-  
-     @Post('signin')
-     signin(){
-        return this.authService.signin();
-     };
-    }
+  constructor(private authService: AuthService) {}
+  // dependencies injection
+  //   this.authService.login();
+  // post request
+  @Post('signup')
+  signup(
+    @Body('email')
+    email: string,
+    @Body('password', ParseIntPipe)
+    password: string,
+  ) {
+    console.log({
+      email,
+      typeOfEmail : typeof email,
+      password,
+      typeOfPassword : typeof password
+    });
+    return this.authService.signup();
+  }
+
+  @Post('signin')
+  signin() {
+    return this.authService.signin();
+  }
+}
